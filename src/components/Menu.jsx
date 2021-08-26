8db535b95ed29f324afd5b77baa6498c3c0e87b6
Abel Souza <abelsouzacosta@gmail.com>
2021-08-26 02:04:58 -0300

    Adicionando o componente Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
