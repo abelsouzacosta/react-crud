5013695156355dca117226d5c6e02c1404df63e8
Abel Souza <abelsouzacosta@gmail.com>
2021-08-26 02:21:43 -0300

    Adicionando estado: livros da aplicação

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import Menu from "./components/Menu";
 import TabelaLivros from "./components/TabelaLivros";
 
 class App extends Component {
+  state = {
+    livros: [
+      {
+        id: 1,
+        isbn: "978-85-7522-403-8",
+        titulo: "HTML5 - 2ª Edição",
+        autor: "Maurício Samy Silva",
+      },
+      {
+        id: 2,
+        isbn: "978-85-7522-807-4",
+        titulo: "Introdução ao Pentest",
+        autor: "Daniel Moreno",
+      },
+      {
+        id: 1,
+        isbn: "978-85-7522-780-8",
+        titulo: "Internet das coisas para desenvolvedores",
+        autor: "Ricardo Da Silva Oligari",
+      },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
